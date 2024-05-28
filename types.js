f1a7da121e919cb1ae485f67b31fdaea90a05636
app samurai <appsamurai@192.168.1.10>
2024-05-28 22:48:02 +0300

    learn: first commit

---
+++ types.js
@@ -0,0 +1,11 @@
+var isDone = false;
+var decimal = 6;
+var color = "blue";
+var list = [1, 2, 3];
+var x;
+x = ["hello", 10]; // Tuple
+console.log(isDone);
+console.log(decimal);
+console.log(color);
+console.log(list);
+console.log(x);
---
+++ types.ts
@@ -0,0 +1,13 @@
+let isDone: boolean = false;
+let decimal: number = 6;
+let color: string = "blue";
+let list: number[] = [1, 2, 3];
+let x: [string, number];
+x = ["hello", 10]; // Tuple
+
+
+console.log(isDone)
+console.log(decimal)
+console.log(color)
+console.log(list)
+console.log(x)
